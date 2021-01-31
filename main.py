37f7bb85067d8e0db0cc63d372d90a2a9de3603f
Matthew Barnes <taking1n1@gmail.com>
2021-01-31 22:49:34 +0000

    Added UI

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+
+app = QApplication(sys.argv)
+
+# If you saved the template in `templates/main_window.ui`
+ui = uic.loadUi("templates/main.ui")
+ui.show()
+
+# Then you can access the objects from the UI
+# For example, if you had a label named label1
+# ui.label1.setText('new text')
+
+sys.exit(app.exec_())
